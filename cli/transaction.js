342feee04fa0c253c202dcbfab994d6b94e33a74
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-07-04 11:11:34 +0100

    Add 'summarize all' CLI command

--- cli/transaction.js
+++ cli/transaction.js
@@ -1,4 +1,8 @@
 import { Command } from 'commander'
+import fs from "fs/promises"
+
+const file = await fs.readFile("data/Transactions2014.csv", "utf-8")
+const fileLines = file.split("\r\n")
 
 const transactionController = new Command('transaction')
 
@@ -9,6 +13,37 @@ transactionController
     const now = new Date()
 
     console.log(`At ${now.toDateString()}, ${from} sent ${to} £${amount}`)
+  })
+
+transactionController
+  .command("summarise all")
+  .description("Summarise all the transactions")
+  .action(() => {
+    const allButFirstLine = fileLines.slice(1)
+    const names = {}
+
+    for (let line of allButFirstLine) {
+      const lineSplit = line.split(",")
+      const fromName = lineSplit[1]
+      const toName = lineSplit[2]
+      const amount = parseFloat(lineSplit[4])
+
+      if (fromName in names) {
+        names[fromName] -= amount
+      } else {
+        names[fromName] = -amount
+      }
+
+      if (toName in names) {
+        names[toName] += amount
+      } else {
+        names[toName] = amount
+      }
+    }
+
+    for (let accountName in names) {
+      console.log(`${accountName}: £${names[accountName].toFixed(2)}`)
+    }
   })
 
 export default transactionController
